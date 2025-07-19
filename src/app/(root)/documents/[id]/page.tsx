655522646730b9943dcbd7c45a341c3634d36f4e
DereckAn <dereckangeles15071998@gmail.com>
2025-07-19 01:59:25 -0600

    feat: add initial Home component and type definitions

--- src/app/(root)/documents/[id]/page.tsx
+++ src/app/(root)/documents/[id]/page.tsx
@@ -1,8 +1,14 @@
+import Header from "@/components/Header";
 import { Editor } from "@/components/editor/Editor";
 
 const DocumentPage = () => {
   return (
     <div>
+      <Header>
+        <div className="flex w-fit items-center justify-center gap-2">
+          <p className="document-title">Document Title</p>
+        </div>
+      </Header>
       <Editor />
     </div>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,24 @@
+import Image from "next/image";
 import Link from "next/link";
 
-const Header = () => {
+interface HeaderProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const Header = ({ children, className }: HeaderProps) => {
   return (
-    <div className="header">
-      <Link href={"/"} className="logo">
-        <h1>Next.js</h1>
+    <div className="header flex">
+      <Link href={"/"} className="logo md:flex-1">
+        <Image
+          src={"/images/pato.webp"}
+          alt="logo"
+          width={100}
+          height={100}
+          className="logo-image"
+        />
       </Link>
+      {children}
     </div>
   );
 };
